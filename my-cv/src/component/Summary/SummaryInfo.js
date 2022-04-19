04194b1e2a61f7116def5d7ddba80ba8732374de
Ovuzo <ovuzonyobeno@gmail.com>
2022-04-19 21:54:26 +0100

    'myy-cv'

---
+++ my-cv/src/component/Summary/SummaryInfo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style.css';
+
+function SummaryInfo() {
+  return (
+        <div className='sum py-1'>
+          <div className='sum-wrap'>
+            <h3  className='py-2'>Summary</h3>
+            <hr></hr>
+            <p className='py-top'>This is my summary it is my summary my summary is my summary it is my summary my summary
+            is my summary it is my summary my summary is my summary it is my summary my summary
+            </p>
+          </div>
+        </div>
+        
+  )
+}
+
+export default SummaryInfo
